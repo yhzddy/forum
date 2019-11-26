f36d44e0ea47185f20eca85cdeed51db72795b6f
yhzddy <1194725376@qq.com>
2019-11-26 23:01:23 +0800

    LoginAndRegister

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">About</router-link>|
+    </div> -->
     <router-view />
   </div>
 </template>
 
 <style lang="less">
+html {
+  font-size: 12px;
+}
+body {
+  font-size: 16px;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RegisterAndLogin msg="注册"> </RegisterAndLogin>
+    <RegisterAndLogin msg="登录"></RegisterAndLogin>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import RegisterAndLogin from "@/components/RegisterAndLogin.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    RegisterAndLogin
+  },
+  data() {
+    return {
+      showlogin: false,
+      showregister: false
+    };
   }
 };
 </script>
